439f0334b88117f8b4634774b3f0f5202aa4567b
DinDing1 <119488409+DinDing1@users.noreply.github.com>
2025-12-20 11:06:53 +0800

    Update imagebuilder.sh

--- config/imagebuilder/imagebuilder.sh
+++ config/imagebuilder/imagebuilder.sh
@@ -140,7 +140,15 @@ custom_packages() {
     [[ "${?}" -eq "0" ]] || echo -e "${INFO} [ ${argon_theme} ] download failed, will use package manager!"
     echo -e "${INFO} The [ ${argon_theme} ] download attempt completed."
     #
-
+    # Download luci-app-openclash
+    openclash_api="https://api.github.com/repos/vernesong/luci-app-openclash/releases"
+    #
+    openclash="luci-app-openclash"
+    openclash_down="$(curl -s ${openclash_api} | grep "browser_download_url" | grep -oE "https.*${openclash}.*.ipk" | head -n 1)"
+    curl -fsSOJL ${openclash_down}
+    [[ "${?}" -eq "0" ]] || echo -e "${INFO} [ ${openclash} ] download failed, will use package manager!"
+    echo -e "${INFO} The [ ${openclash} ] download attempt completed."
+    #
 
     # Download other luci-app-xxx
     # ......
@@ -203,10 +211,10 @@ rebuild_firmware() {
         luci luci-base luci-compat luci-i18n-base-zh-cn luci-lib-base luci-lib-docker \
         luci-lib-ip luci-lib-ipkg luci-lib-jsonc luci-lib-nixio luci-mod-admin-full luci-mod-network \
         luci-mod-status luci-mod-system luci-proto-3g luci-proto-ipip luci-proto-ipv6 \
-        luci-proto-ncm luci-proto-openconnect luci-proto-ppp luci-proto-qmi luci-proto-relay \
+        luci-proto-ncm luci-proto-openconnect luci-proto-ppp luci-proto-qmi luci-proto-relay iptables dnsmasq-full ca-bundle ipset ip-full iptables-mod-tproxy iptables-mod-extra ruby ruby-yaml kmod-tun kmod-inet-diag \
         \
-        luci-app-amlogic luci-i18n-amlogic-zh-cn luci-theme-argon \
-        \
+        luci-app-amlogic luci-i18n-amlogic-zh-cn luci-theme-argon luci-app-openclash \
+        \ 
         ${config_list} \
         "
 
